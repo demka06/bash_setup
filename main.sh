180bbf521ade6482f5b69f8789cc2f3837f6045c
Александр <59875707+demka06@users.noreply.github.com>
2025-08-14 16:05:39 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -5,6 +5,9 @@ if [[ $EUID -ne 0 ]]; then
     exit 1
 fi
 
+sudo chmod +x ./src/setup.sh
+sudo chmod +x ./src/add_user.sh
+
 while true; do
     echo "Выберите действие:"
     echo "1) Сконфигурировать сервер"
